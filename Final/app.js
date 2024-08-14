73605e6fdbb987d82a368777809c567af419ff7e
lgjoel <lg.joel733@gmail.com>
2024-08-13 20:42:14 -0400

    examen
    final

---
+++ Final/app.js
@@ -0,0 +1,13 @@
+let nota=67
+switch (true){
+    case nota >= 60:
+        console.log('Aprobado');
+        break;
+    case nota <60 :
+        console.log('Reprobado');
+        break;
+    default:
+        console.log('Nota no admisible');
+        break;
+}
+
